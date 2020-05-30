7c9bf46e2cee944e4495d89d3882a8bdad2390ab
Predyy-10 <sahrawat.anant@gmail.com>
2020-05-30 16:11:15 +0530

    destini challenge

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,52 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [];
+
+  void addIcons(bool check) {
+    setState(() {
+      if (quizBrain.getQuestionAnswer() == check) {
+        scorekeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        scorekeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+      quizBrain.nextQuestion();
+      gameOver();
+    });
+  }
+
+  void gameOver() {
+    bool checkFinished = quizBrain.isFinished();
+    setState(() {
+      if (checkFinished) {
+        Alert(
+          type: AlertType.error,
+          context: context,
+          title: "GAMEOVER !!!",
+          desc: "Hope you had Fun !!",
+          buttons: [
+            DialogButton(
+              child: Text("Try Again"),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        ).show();
+        quizBrain.resetQuestions();
+        scorekeeper.clear();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +87,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +112,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                addIcons(true);
               },
             ),
           ),
@@ -79,12 +130,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                addIcons(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scorekeeper,
+        ),
       ],
     );
   }
